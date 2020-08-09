a1602481632f35d1216984ad8f00be64e2233372
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-08-09 16:21:35 +0900

    Add scroll view in main activity

--- app/src/main/java/com/kwang0/stickyscrollview/MainActivity.kt
+++ app/src/main/java/com/kwang0/stickyscrollview/MainActivity.kt
@@ -2,10 +2,24 @@ package com.kwang0.stickyscrollview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.simpleName.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sticky_scroll_view.run {
+            header = header_view
+            stickListener = { _ ->
+                Log.d(TAG, "stickListener")
+            }
+            freeListener = { _ ->
+                Log.d(TAG, "freeListener")
+            }
+        }
     }
 }
